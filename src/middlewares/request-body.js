3f84bee9bac76e4bdac9303f16c10719d079a7ab
ymillerr <millerandvitor@gmail.com>
2023-11-12 15:10:37 +0100

    feat: ensuring that the entire body of the request is consumed

---
+++ src/middlewares/request-body.js
@@ -0,0 +1,15 @@
+export async function requestBody(req, res){
+  const chuncks = []
+
+  for await (const chunck of req) {
+    chuncks.push(chunck)
+  }
+
+  const fullContentStream = Buffer.concat(chuncks).toString()
+
+  try {
+    req.body = JSON.parse(fullContentStream)
+  } catch {
+    req.body = {}
+  }
+}
