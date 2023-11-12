9f372d08a28e973ed7e7c79c693e269bf88f59b0
ymillerr <millerandvitor@gmail.com>
2023-11-12 12:15:37 +0100

    feat: add method persist at database

--- src/database/database.js
+++ src/database/database.js
@@ -1,11 +1,21 @@
+import fs from 'node:fs'
+
+const databaseFilePath = new URL("../../db.json", import.meta.url)
+
 export class Database {
   #database = {}
+
+  #persist() {
+    fs.writeFile(databaseFilePath, JSON.stringify(this.#database))
+  }
 
   create(table, data) {
     if(Array.isArray(this.#database[table])) {
       this.#database[table].push(data)
+      return this.#persist()
     }
 
     this.#database[table] = data
+    this.#persist()
   }
 }
