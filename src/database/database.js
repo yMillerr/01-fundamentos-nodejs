1dd79ca6a1b4a9cc92481e92af100d53ec3eaa9d
ymillerr <millerandvitor@gmail.com>
2023-11-12 12:12:09 +0100

    feat: add method create at dabase

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,11 @@
 export class Database {
   #database = {}
 
+  create(table, data) {
+    if(Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    }
 
+    this.#database[table] = data
+  }
 }
