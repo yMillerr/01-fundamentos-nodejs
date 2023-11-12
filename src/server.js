d2d723e6beadae4b8fad2b4feaea2cad990069bd
ymillerr <millerandvitor@gmail.com>
2023-11-12 15:26:00 +0100

    feat: create the task creation method

--- src/server.js
+++ src/server.js
@@ -1,10 +1,31 @@
 import http from 'node:http'
 
-const server = http.createServer((req, res) => {
-  res.end(JSON.stringify({
-    message: 'Hello world'
-  }))
+import { Database } from './database/Database.js';
+import { requestBody } from './middlewares/request-body.js';
+const database = new Database()
+
+import { randomUUID } from 'node:crypto'
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await requestBody(req, res)
+
+  if (method === 'POST' && url === '/tasks') {
+    const { title, description } = req.body
+
+    const task = {
+      id: randomUUID(),
+      title,
+      description
+    }
+
+    database.create('tasks', task)
+    
+    return res.end()
+  }
+  
+  res.end();
 })
 
 server.listen(3333)
-
