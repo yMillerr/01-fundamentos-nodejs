aa63de91b7a13503482be43db7ff0e51f3455fc5
ymillerr <millerandvitor@gmail.com>
2023-11-13 16:13:49 +0100

    feat: create update route

--- src/routes/route-handlers.js
+++ src/routes/route-handlers.js
@@ -1,12 +1,13 @@
 import { Database } from '../database/Database.js'
 import { randomUUID } from 'node:crypto'
+import { buildRoutePath } from '../utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'POST',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
     const { title, description } = req.body
 
@@ -23,11 +24,27 @@ export const routes = [
   },
   {
     method: 'GET',
-    path: '/tasks',
+    path: buildRoutePath('/tasks'),
     handler: (req, res) => {
       const tasks = database.read('tasks')
 
       return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body
+
+      database.update('tasks', id , {
+        id,
+        title,
+        description
+      })
+
+      res.end()
     }
   }
 ]
